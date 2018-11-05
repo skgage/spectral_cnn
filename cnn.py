6d940ad2ffc409b75ee4bb0e56166e72ff5f4abb
Sarah Gage <sarah.gage@colorado.edu>
2018-11-05 12:22:21 -0700

    initializing spectral cnn code file

---
+++ cnn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 12:14:26 2018
+
+@author: sarahgage
+"""
+
+#--------Imports--------------------------------------
+import tensorflow as tf
+import numpy as np
+import math
+import matplotlib.pyplot as plt
